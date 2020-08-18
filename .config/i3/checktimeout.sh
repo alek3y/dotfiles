ea72214999f3bc14a29995c8a8f0e33f83f55899
alek3y <alek3y@protonmail.com>
2020-08-18 14:44:25 +0200

    🚧 Update checktimeout.sh
    Other than checking only for running audio, check also that the lockscreen is not already running.

--- .config/i3/checktimeout.sh
+++ .config/i3/checktimeout.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Check if there's no audio running on soundcards
-if [[ $(grep -R -i running /proc/asound | wc -l) == 0 ]]; then
+# Count how many processes are running sound
+soundprocs=$(grep -R -i running /proc/asound | wc -l)
+
+# Count how many i3lock processes are running
+lockprocs=$(ps axH | grep -vi grep | grep -i i3lock | wc -l)
+
+# Check that no processes are using the audio and
+# that the lockscreen isn't already running
+if [[ $soundprocs == 0 && $lockprocs == 0 ]]; then
 	bash $HOME/.config/i3/lockscreen.sh
 fi
