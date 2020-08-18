c73b9a0b5b918f0a33902052c102a5b2666fb8c2
alek3y <alek3y@protonmail.com>
2020-08-18 18:15:01 +0200

    ✨ Customize workspaces

--- .config/i3/lockscreen.sh
+++ .config/i3/lockscreen.sh
@@ -4,7 +4,7 @@
 # Colors
 #
 
-background="$HOME/Pictures/wallpaper.png"
+wallpaper="$HOME/Pictures/lockscreen.png"
 backgroundcolor="000000ff"
 ringcolor="ffffffff"
 insidecolor="ffffff00"
@@ -23,7 +23,7 @@ i3lock \
         --color=$backgroundcolor \
         --pointer default \
         --force-clock \
-        --image $background --centered \
+        --image="$wallpaper" --centered \
         --ignore-empty-password \
         --insidecolor=$insidecolor --insidevercolor=$insidecolor --insidewrongcolor=$insidecolor \
         --ringcolor=$ringcolor --ringvercolor=$verifyingcolor --ringwrongcolor=$activecolor \
@@ -32,8 +32,8 @@ i3lock \
         --separatorcolor=$activecolor \
         --veriftext="" --wrongtext="" --noinputtext="" --locktext="" --lockfailedtext="Error" \
         --wrongcolor=$ringcolor \
-        --timecolor=$timecolor --timestr="%H:%M" --timesize="70" --time-font=$font \
-        --datecolor=$datecolor --datestr="Type password.." --datesize=20 --date-font=$font \
+        --timecolor=$timecolor --timestr="%H:%M" --timesize="70" --time-font="$font" \
+        --datecolor=$datecolor --datestr="Type password.." --datesize=20 --date-font="$font" \
         --radius=20 --ring-width=5 \
         --time-align=1 --date-align=1 \
         --indpos="x+50+200+32+r:y+h-65-(86/2)" --timepos="x+50:y+h-100" --datepos="x+50+5+2:y+h-65"
