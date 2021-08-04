a0e77d99033fd429420592f98ddabf7e5f73b12e
alek3y <alek3y@protonmail.com>
2021-08-04 21:26:22 +0200

    👽️ Update i3lock-color parameters

--- .config/i3/lockscreen.sh
+++ .config/i3/lockscreen.sh
@@ -25,15 +25,15 @@ i3lock \
         --force-clock \
         --image="$wallpaper" --centered \
         --ignore-empty-password \
-        --insidecolor=$insidecolor --insidevercolor=$insidecolor --insidewrongcolor=$insidecolor \
-        --ringcolor=$ringcolor --ringvercolor=$verifyingcolor --ringwrongcolor=$activecolor \
+        --inside-color=$insidecolor --insidever-color=$insidecolor --insidewrong-color=$insidecolor \
+        --ring-color=$ringcolor --ringver-color=$verifyingcolor --ringwrong-color=$activecolor \
         --line-uses-inside \
-        --keyhlcolor=$activecolor --bshlcolor=$activecolor \
-        --separatorcolor=$activecolor \
-        --veriftext="" --wrongtext="" --noinputtext="" --locktext="" --lockfailedtext="Error" \
-        --wrongcolor=$ringcolor \
-        --timecolor=$timecolor --timestr="%H:%M" --timesize="70" --time-font="$font" \
-        --datecolor=$datecolor --datestr="Type password.." --datesize=20 --date-font="$font" \
+        --keyhl-color=$activecolor --bshl-color=$activecolor \
+        --separator-color=$activecolor \
+        --verif-text="" --wrong-text="" --noinput-text="" --lock-text="" --lockfailed-text="Error" \
+        --wrong-color=$ringcolor \
+        --time-color=$timecolor --time-str="%H:%M" --time-size="70" --time-font="$font" \
+        --date-color=$datecolor --date-str="Type password.." --date-size=20 --date-font="$font" \
         --radius=20 --ring-width=5 \
         --time-align=1 --date-align=1 \
-        --indpos="x+50+200+32+r:y+h-65-(86/2)" --timepos="x+50:y+h-100" --datepos="x+50+5+2:y+h-65"
+        --ind-pos="x+50+200+32+r:y+h-65-(86/2)" --time-pos="x+50:y+h-100" --date-pos="x+50+5+2:y+h-65"
