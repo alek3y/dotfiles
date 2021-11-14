8fb1afd4a23b236b8a1cbc5f94d24c512407b4c2
alek3y <alek3y@protonmail.com>
2021-11-14 20:42:00 +0100

    Change dwm accent color

--- wm/dwm/config.def.h
+++ wm/dwm/config.def.h
@@ -10,20 +10,20 @@ static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows sel
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { FONT };
 static const char dmenufont[]       = FONT;
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char borders[]         = "#444444";
+static const char tags_fg[]         = "#bbbbbb";
+static const char tags_bg[]         = "#222222";
+static const char title_fg[]        = "#eeeeee";
+static const char accent[]          = "#bf7737";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg        bg       border */
+	[SchemeNorm] = { tags_fg,  tags_bg, borders },
+	[SchemeSel]  = { title_fg, accent,  accent  },
 };
 
 /* tagging */
@@ -65,7 +65,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", tags_bg, "-nf", tags_fg, "-sb", accent, "-sf", title_fg, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *shutdowncmd[] = { "systemctl", "poweroff", NULL };
 
