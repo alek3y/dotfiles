7bfb7582a8b0706a73a41d9d8166707ece501dee
alek3y <alek3y@protonmail.com>
2021-11-25 18:15:14 +0100

    Comment buttons on actions

--- wm/.local/opt/wm/status/volume.sh
+++ wm/.local/opt/wm/status/volume.sh
@@ -10,21 +10,32 @@ volume=$(pamixer --get-volume)
 
 button=$1
 case $button in
+
+	# Left click
 	1)
 		pamixer -t
 		;;
+
+	# Middle click
 	2)
 		pamixer --set-volume $DEFAULT
 		;;
+
+	# Right click
 	3)
 		pavucontrol
 		;;
+
+	# Scroll up (natural scrolling)
 	4)
 		pamixer -d $STEP
 		;;
+
+	# Scroll down (natural scrolling)
 	5)
 		pamixer -i $STEP
 		;;
+
 	*)
 		if pamixer --get-mute >/dev/null; then
 			icon=$MUTED
