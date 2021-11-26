fd56cfc2900769af2a69ec6bfd66efaa7b2006a8
alek3y <alek3y@protonmail.com>
2021-11-26 21:09:50 +0100

    Add battery status check

--- wm/.local/opt/wm/status/battery.sh
+++ wm/.local/opt/wm/status/battery.sh
@@ -7,11 +7,16 @@ readonly LEVELS=("" "" "" "" "" "" "" "" "" "")
 readonly MISSING=""
 
 level=$(acpi -b | grep -ioP '[0-9]+(?=%)')
+status=$(acpi -b | grep -ioP '(dis)?charging' | tr '[:upper:]' '[:lower:]')
 
-if [[ -z $level ]]; then
+if [[ -z $level || -z $status ]]; then
 	echo "$MISSING N/A"
 	exit 1
 fi
 
-icon_index=$(map $level 0 100 0 $((${#LEVELS[@]} - 1)))
-echo "${LEVELS[$icon_index]} $level%"
+if [[ $status == "discharging" ]]; then
+	icon_index=$(map $level 0 100 0 $((${#LEVELS[@]} - 1)))
+	echo "${LEVELS[$icon_index]} $level%"
+else
+	echo "$CHARGING $level%"
+fi
